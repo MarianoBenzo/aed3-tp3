d64a748dd4da9f196f4f46ef8b72e2e9bcf2bafa
MarianoBenzo <marianobenzo@hotmail.com>
2017-06-22 17:12:14 -0300

    nuevo goloso

--- CMFgoloso.java
+++ CMFgoloso.java
@@ -6,15 +6,53 @@ public class CMFgoloso
     public static int CMFgoloso(boolean[][] grafo, int n)
     {	
 
-        int nodoMayorGrado = funcionesAux.nodoMayorGrado(grafo, n);
+        int nodoInicial = funcionesAux.nodoMayorGrado(grafo, n);
+        //int nodoInicial = (int) (Math.random() * n);
 
-        return funcionesAux.CMFnodo(grafo, n, nodoMayorGrado);
+        List<Integer> adyacentes = funcionesAux.nodosAdyacentes(grafo, n, nodoInicial);
+
+        List<Integer> clique = new ArrayList<Integer>();
+        clique.add(nodoInicial);
+
+        List<Integer> cliqueMaxFrontera = new ArrayList<Integer>();
+        cliqueMaxFrontera.add(nodoInicial);
+
+        int maxFrontera = adyacentes.size();
+
+        boolean fin = false;
+
+        while(!fin)
+        {   
+            fin = true;
+            for(int adyacente : adyacentes)
+            {
+                if(funcionesAux.formaClique(grafo, clique, adyacente))
+                {
+                    List<Integer> cliqueAux = new ArrayList<Integer>(clique);
+                    cliqueAux.add(adyacente);
+
+                    int fronteraAux = funcionesAux.frontera(grafo, n, cliqueAux);
+
+                    if(fronteraAux > maxFrontera)
+                    {
+                        maxFrontera = fronteraAux;
+                        cliqueMaxFrontera = cliqueAux;
+
+                        fin = false;
+                    }
+                }
+            }
+            clique = cliqueMaxFrontera;
+        }
+
+
+        return maxFrontera;
     }
 
 
     public static void main(String[] args)
     {
-        int n = 100;
+        int n = 200;
         
         int m = n*(n-1)/8;
 
