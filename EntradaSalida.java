1cd99d28c8754a99918cfb151344a71095dbe2e1
Christian Bonomi <clbonomi@gmail.com>
2017-06-23 20:52:35 -0300

    Clase para escribir resultados a un archivo

---
+++ EntradaSalida.java
@@ -0,0 +1,30 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class EntradaSalida {
+	
+    public static void escribirTiempos(List<Long> tiempos, String nombreArchivo) {
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try {
+            fichero = new FileWriter(nombreArchivo);
+            pw = new PrintWriter(fichero);
+            
+            for (Long tiempo : tiempos) {
+                pw.println(tiempo);	
+			}
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+    }
+        
+    
+}
